4d0f94a8f3dd6ff1e325f154622189ff0a4315ed
김문규 <heal@gimmungyus-MacBook-Pro.local>
2024-05-27 15:06:36 +0900

    9

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,16 @@
 import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { AppService } from './app.service';
 import axios from 'axios';
 import {stringify} from 'qs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 
   @Post('nice-test')
   async niceTest() {
@@ -50,71 +50,71 @@ export class AppController {
     if (!username || !password) {
       return res.status(400).json({message: "input username, password necesarry"})
     }
-    const result =  await this.appService.signup(username, password);
-    if (result === 'username already exists') {
-      return res.status(400).json({message: "username already exists"})
-    }
+    // const result =  await this.appService.signup(username, password);
+    // if (result === 'username already exists') {
+    //   return res.status(400).json({message: "username already exists"})
+    // }
 
     return res.status(200).json();
   }
   
-  @Post('auth/signin')
-  async signin(
-    @Body() { username, password },
-    @Res() res
-  ) {
-    if (!username || !password) {
-      return res.status(400).json({message: "input username, password necesarry"})
-    }
-    const result = await this.appService.signin(username, password);
-    if (result === "non existing user") return res.status(400).json({message: "non existing user"})
-    if (result === "wrong password") return res.status(400).json({message: "wrong password"})
-    console.log(result)
-    console.log(result)
-    console.log(result)
-    return res.status(200).json()
-  }
+  // @Post('auth/signin')
+  // async signin(
+  //   @Body() { username, password },
+  //   @Res() res
+  // ) {
+  //   if (!username || !password) {
+  //     return res.status(400).json({message: "input username, password necesarry"})
+  //   }
+  //   const result = await this.appService.signin(username, password);
+  //   if (result === "non existing user") return res.status(400).json({message: "non existing user"})
+  //   if (result === "wrong password") return res.status(400).json({message: "wrong password"})
+  //   console.log(result)
+  //   console.log(result)
+  //   console.log(result)
+  //   return res.status(200).json()
+  // }
   
-  @Get('chatroom')
-  async getChatroom() {
-    return this.appService.getChatroom();
-  }
+  // @Get('chatroom')
+  // async getChatroom() {
+  //   return this.appService.getChatroom();
+  // }
   
-  @Post('chatroom')
-  async createChatroom(
-    @Body() { name }
-  ) {
-    return this.appService.createChatroom(name);
-  }
+  // @Post('chatroom')
+  // async createChatroom(
+  //   @Body() { name }
+  // ) {
+  //   return this.appService.createChatroom(name);
+  // }
   
-  @Post('message')
-  async createMessage(
-    @Body() { chatroomId, content }
-  ) {
-    return this.appService.createMessage(chatroomId, content);
-  }
+  // @Post('message')
+  // async createMessage(
+  //   @Body() { chatroomId, content }
+  // ) {
+  //   return this.appService.createMessage(chatroomId, content);
+  // }
   
-  @Get('message')
-  async getMessage(
-    @Body() { chatroomId }
-  ) {
-    return this.appService.getMessages(chatroomId);
-  }
+  // @Get('message')
+  // async getMessage(
+  //   @Body() { chatroomId }
+  // ) {
+  //   return this.appService.getMessages(chatroomId);
+  // }
 
-  @Post('kakaopay/payment/ready')
-  async tryKakaoPaymentReady(@Body() body) {
-    try {
-      const response = await axios.post('https://open-api.kakaopay.com/online/v1/payment/ready', body, {
-        headers: {
-          Authorization: "SECRET_KEY DEV057F63A391B978F98352FAC04D7F4C488A9B1",
-          "Content-Type": "application/json"
-        }
-      });
-      return response.data;
-    } catch (err) {
-      return err;
-    }
-  }
+  // @Post('kakaopay/payment/ready')
+  // async tryKakaoPaymentReady(@Body() body) {
+  //   try {
+  //     const response = await axios.post('https://open-api.kakaopay.com/online/v1/payment/ready', body, {
+  //       headers: {
+  //         Authorization: "SECRET_KEY DEV057F63A391B978F98352FAC04D7F4C488A9B1",
+  //         "Content-Type": "application/json"
+  //       }
+  //     });
+  //     return response.data;
+  //   } catch (err) {
+  //     return err;
+  //   }
+  // }
   
   
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,88 +1,88 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaClient } from '@prisma/client';
+// import { Injectable } from '@nestjs/common';
+// // import { PrismaService } from './prisma/prisma.service';
+// // import { PrismaClient } from '@prisma/client';
 
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+// @Injectable()
+// export class AppService {
+//   getHello(): string {
+//     return 'Hello World!';
+//   }
   
-  async signup(username, password) {
-    const prismaClient = new PrismaService();
-    try {
-      return await prismaClient.user.create({
-        data: {
-          username,
-          password
-        }
-      })
-    } catch (err) {
-      if (err.code == 'P2002') return 'username already exists'
-    }
-  }
+//   async signup(username, password) {
+//     // const prismaClient = new PrismaService();
+//     // try {
+//     //   return await prismaClient.user.create({
+//     //     data: {
+//     //       username,
+//     //       password
+//     //     }
+//     //   })
+//     // } catch (err) {
+//     //   if (err.code == 'P2002') return 'username already exists'
+//     // }
+//   }
   
-  async signin(username, password) {
-    const prismaClient = new PrismaService();
-      const user = await prismaClient.user.findFirst({
-        where: {
-          username
-        }
-      })
-        console.log(user)
-        console.log(user)
-        console.log(user)
-      if (!user) return "non existing user";
+//   async signin(username, password) {
+//     const prismaClient = new PrismaService();
+//       const user = await prismaClient.user.findFirst({
+//         where: {
+//           username
+//         }
+//       })
+//         console.log(user)
+//         console.log(user)
+//         console.log(user)
+//       if (!user) return "non existing user";
 
-      if (user.password !== password) {
-        return "wrong password"
-      }
+//       if (user.password !== password) {
+//         return "wrong password"
+//       }
 
-      return user
-  }
+//       return user
+//   }
 
-  async getChatroom() {
-    const prismaClient = new PrismaService();
+//   async getChatroom() {
+//     const prismaClient = new PrismaService();
 
-    const chatrooms = await prismaClient.chatroom.findMany();
-    return chatrooms;
-  }
+//     const chatrooms = await prismaClient.chatroom.findMany();
+//     return chatrooms;
+//   }
   
-  async createChatroom(name) {
-    const prismaClient = new PrismaService();
+//   async createChatroom(name) {
+//     const prismaClient = new PrismaService();
 
-    const chatrooms = await prismaClient.chatroom.create({
-      data: {
-        name: name
-      }
-    });
-    return chatrooms;
-  }
+//     const chatrooms = await prismaClient.chatroom.create({
+//       data: {
+//         name: name
+//       }
+//     });
+//     return chatrooms;
+//   }
   
-  async createMessage(chatroomId, content) {
-    const prismaClient = new PrismaService();
+//   async createMessage(chatroomId, content) {
+//     const prismaClient = new PrismaService();
 
-    const chatrooms = await prismaClient.message.create({
-      data: {
-        content: content,
-        chatroomId: chatroomId
-      }
-    });
-    return chatrooms;
-  }
+//     const chatrooms = await prismaClient.message.create({
+//       data: {
+//         content: content,
+//         chatroomId: chatroomId
+//       }
+//     });
+//     return chatrooms;
+//   }
   
-  async getMessages(chatroomId) {
-    const prismaClient = new PrismaService();
+//   async getMessages(chatroomId) {
+//     const prismaClient = new PrismaService();
 
-    const chatrooms = await prismaClient.message.findMany({
-      where: {
-        chatroomId: chatroomId
-      },
-      // sort
-      orderBy: {
-        createdAt: 'desc'
-      }
-    });
-    return chatrooms;
-  }
-}
+//     const chatrooms = await prismaClient.message.findMany({
+//       where: {
+//         chatroomId: chatroomId
+//       },
+//       // sort
+//       orderBy: {
+//         createdAt: 'desc'
+//       }
+//     });
+//     return chatrooms;
+//   }
+// }
