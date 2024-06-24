60d22e4ef69b6bc48817f9ee22bf1c17b7d26ad9
김문규 <heal@gimmungyus-MacBook-Pro.local>
2024-06-24 14:02:09 +0900

    express-session

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 import {stringify} from 'qs';
 import { v4 as uuidv4 } from 'uuid';
 import { createHash, createCipheriv, createHmac} from 'crypto';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(4001);
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  
 }
 
 bootstrap();
